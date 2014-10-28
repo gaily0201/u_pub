7b6032c284c91a1383884c4223ad244263b1d474
gaily <gaily0201@163.com>
2014-10-28 19:25:26 +0800

    extract add method

--- pub/src/public/cn/gaily/pub/trigger/TriggerBaseTask.java
+++ pub/src/public/cn/gaily/pub/trigger/TriggerBaseTask.java
@@ -52,100 +52,116 @@ public class TriggerBaseTask {
 	private Map<String,Map<String,String>> tabColMap = new HashMap<String,Map<String,String>>();
 	
 	/**
-	 * <p>方法名称：TriggerBaseTask</p>
-	 * <p>方法描述：构造函数，初始化远程和本地数据库连接池</p>
-	 * @author xiaoh
-	 * @since  2014-10-28
-	 * <p> history 2014-10-28 xiaoh  创建   <p>
+	 * 保存列值，Map<String,Object> key:列名, value:列值
 	 */
-	public TriggerBaseTask(){
-//		if(remote.conns.isEmpty()){
-//			remote.setInitSize(DSSIZE);
-//			remote.initDB("orcl", "192.168.1.100", "uap63", "1", "1521");
-//			remote.init();
-//		}
-//		if(local.conns.isEmpty()){
-//			local.setInitSize(DSSIZE);
-//			local.initDB("orcl", "192.168.1.100", "uap63_test", "1", "1521");
-//			local.init();
-//		}
+	private List<Map<String,Object>> valueList 		= new ArrayList<Map<String,Object>>(); //值list,map中保存<字段名，字段值>
+	/**
+	 * 操作的pk值，以备回删数据
+	 */
+	private List<String> 			 pkValues 		= new ArrayList<String>();
+	/**
+	 * 列名和类型对应关系
+	 */
+	private Map<String,String> 		 colNameTypeMap	= new HashMap<String,String>();
+	/**
+	 * 列和索引映射map,值插入设值
+	 */
+	private Map<String,Integer> 	 colIndexMap 	= new HashMap<String,Integer>();
+	
+	
+	
+	private TriggerBaseTask(){
 	}
 	
+	public TriggerBaseTask getInstance(){
+		synchronized (this) {
+			return new TriggerBaseTask();
+		}
+	}
+	
+	public void clear(){
+		valueList.clear();
+		pkValues.clear();
+		colNameTypeMap.clear();
+		colIndexMap.clear();
+		
+	}
 	
 	/**
 	 * <p>方法名称：dealAdd</p>
 	 * <p>方法描述：处理新增数据</p>
-	 * <p>
-	 * 		步骤：
-	 * 			1、查询源库临时表新增数据
-	 * 			2、停用目标库触发器
-	 * 			3、执行目标库数据库插入
-	 * 			4、启用目标库触发器
-	 * 			5、删除源库临时表新增数据
-	 * </p>
 	 * @param tableName 表名
 	 * @author xiaoh
 	 * @since  2014-10-28
 	 * <p> history 2014-10-28 xiaoh  创建   <p>
 	 */
 	public void dealAdd(SimpleDSMgr srcMgr, SimpleDSMgr tarMgr, String tableName){
+		clear();
 		
 		if(srcMgr==null||tarMgr==null||CommonUtil.isEmpty(tableName)){
 			throw new RuntimeException("新增数据库操作参数出错");
 		}
-		Map<String,String> colMap = getTabCols(srcMgr, tableName, false);
-		//1、查询源库临时表新增数据
-		StringBuilder querySrcSb = new StringBuilder("SELECT ");
-
-		for(Iterator it=colMap.entrySet().iterator();it.hasNext();){
-			querySrcSb.append(((Entry<String,String>)it.next()).getKey()).append(",");
-		}
-		querySrcSb.deleteCharAt(querySrcSb.length()-1);
-		querySrcSb.append(" FROM ").append(tablePrefix).append(tableName);
-		querySrcSb.append(" WHERE ETLSTATUS=1 ORDER BY ETLTS ASC");
-		
-		Connection srcConn = srcMgr.getConnection();
-		Statement st = null;
-		ResultSet rs = null;
-		List<Map<String,Object>> valueList = new ArrayList<Map<String,Object>>(); //值list,map中保存<字段名，字段值>
-		Map<String,Object> valueMap = new HashMap<String,Object>();
-		List<String> pks = new ArrayList<String>();
+		colNameTypeMap = getTabCols(srcMgr, tableName, false);
 		String pkName = null;
-		try {
-			st  = srcConn.createStatement();
-			rs 	= st.executeQuery(querySrcSb.toString());
-			String colName = null;
-			while(rs.next()){
-				valueMap= new HashMap<String,Object>();
-				for(Iterator it=colMap.entrySet().iterator();it.hasNext();){
-					colName = ((Entry<String,String>)it.next()).getKey();
-					valueMap.put(colName,rs.getString(colName));
-				}
-				valueList.add(valueMap);
-			}
-			//获取pk值
-			for(int i=0;i<valueList.size();i++){
-				pkName = (String) valueList.get(i).get("ETLPKNAME");
-				pks.add((String) valueList.get(i).get(pkName));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally{
-			JdbcUtils.release(null, st, rs);
-		}
+		//1、查询源库临时表新增数据
+		pkName = queryTempData(tableName, srcMgr, 1);
 		
 		//2、停用目标库触发器
-		enableTrigger(tarMgr, tableName, 0, true);
+		enableTrigger(tarMgr, tableName, 0);
 		
 		//3、执行目标库数据库插入
+		doAddInsert(tarMgr, tableName);
+		
+		//4、启用目标库触发器
+		enableTrigger(tarMgr, tableName, 1);
+		
+		//5、删除源库临时表新增数据
+		delTempData(srcMgr, tableName, pkName, 1);
+	}
+
+	/**
+	 * 删除源库临时表数据
+	 * @param srcMgr
+	 * @param tableName
+	 * @param pkName
+	 * @param srcConn
+	 */
+	private void delTempData(SimpleDSMgr srcMgr, String tableName, String pkName, int type) {
+		Connection srcConn = srcMgr.getConnection();
+		Statement delSt = null;
+		StringBuilder delSb = new StringBuilder("DELETE FROM ");
+		delSb.append(tablePrefix).append(tableName);
+		delSb.append(" WHERE ").append(pkName).append(" IN(");
+		for(int i=0;i<pkValues.size();i++){
+			delSb.append("'").append(pkValues.get(i)).append("',");
+		}
+		delSb.deleteCharAt(delSb.length()-1);
+		delSb.append(") AND ETLSTATUS=").append(type);
+		try {
+			delSt = srcConn.createStatement();
+			delSt.execute(delSb.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			JdbcUtils.release(null, delSt, null);
+			srcMgr.release(srcConn);
+		}
+	}
+
+	/**
+	 * 执行目标库数据库插入
+	 * @param tarMgr
+	 * @param tableName
+	 */
+	private void doAddInsert(SimpleDSMgr tarMgr, String tableName) {
 		StringBuilder tarSb = new StringBuilder();
 		tarSb.append("INSERT INTO ").append(tableName).append("(");
 		String colName = null;
 		String colType = null;
 		Entry<String,String> entry =  null;
-		Map<String,Integer> colIndexMap = new HashMap<String,Integer>();
+		
 		int n=0;
-		for(Iterator it=colMap.entrySet().iterator();it.hasNext();){
+		for(Iterator it=colNameTypeMap.entrySet().iterator();it.hasNext();){
 			entry = (Entry<String, String>) it.next();
 			colName = entry.getKey();
 			if(!"ETLSTATUS".equalsIgnoreCase(colName)&&!"ETLPKNAME".equalsIgnoreCase(colName)&&!"ETLTS".equalsIgnoreCase(colName)){
@@ -155,7 +171,7 @@ public class TriggerBaseTask {
 		}
 		tarSb.deleteCharAt(tarSb.length()-1);
 		tarSb.append(") VALUES(");
-		for(int i=0;i<colMap.size()-3;i++){
+		for(int i=0;i<colNameTypeMap.size()-3;i++){
 			tarSb.append("?,");
 		}
 		tarSb.deleteCharAt(tarSb.length()-1);
@@ -168,7 +184,7 @@ public class TriggerBaseTask {
 			targetConn.setAutoCommit(false);
 			ipst = targetConn.prepareStatement(insertSql);
 			for(int i=0;i<valueList.size();i++){
-				for(Iterator it=colMap.entrySet().iterator();it.hasNext();){
+				for(Iterator it=colNameTypeMap.entrySet().iterator();it.hasNext();){
 					entry = (Entry<String, String>) it.next();
 					colName = entry.getKey();
 					colType = entry.getValue();
@@ -212,29 +228,60 @@ public class TriggerBaseTask {
 			throw new RuntimeException("插入数据库异常");
 		}finally{
 			JdbcUtils.release(null, ipst, null);
+			tarMgr.release(targetConn);
 		}
+	}
+
+
+	/**
+	 * 查询数据，返回主键字段
+	 * @param tableName
+	 * @param srcMgr
+	 * @param type
+	 * @return
+	 */
+	private String queryTempData(String tableName,SimpleDSMgr srcMgr, int type) {
+		Map<String, Object> valueMap = null;
+		String pkName = null;
+		Connection srcConn = srcMgr.getConnection();
 		
-		//4、启用目标库触发器
-		enableTrigger(tarMgr, tableName, 1, true);
-		
-		//5、删除源库临时表新增数据
-		StringBuilder delSb = new StringBuilder("DELETE FROM ");
-		delSb.append(tablePrefix).append(tableName);
-		delSb.append(" WHERE ").append(pkName).append(" IN(");
-		for(int i=0;i<pks.size();i++){
-			delSb.append("'").append(pks.get(i)).append("',");
+		StringBuilder querySrcSb = new StringBuilder("SELECT ");
+
+		for(Iterator it=colNameTypeMap.entrySet().iterator();it.hasNext();){
+			querySrcSb.append(((Entry<String,String>)it.next()).getKey()).append(",");
 		}
-		delSb.deleteCharAt(delSb.length()-1);
-		delSb.append(") AND ETLSTATUS=1");
+		querySrcSb.deleteCharAt(querySrcSb.length()-1);
+		querySrcSb.append(" FROM ").append(tablePrefix).append(tableName);
+		querySrcSb.append(" WHERE ETLSTATUS=").append(type);
+		querySrcSb.append(" ORDER BY ETLTS ASC");
+		
+		Statement st = null;
+		ResultSet rs = null;
 		try {
-			st = srcConn.createStatement();
-			st.execute(delSb.toString());
+			st  = srcConn.createStatement();
+			rs 	= st.executeQuery(querySrcSb.toString());
+			String colName = null;
+			while(rs.next()){
+				valueMap= new HashMap<String,Object>();
+				for(Iterator it=colNameTypeMap.entrySet().iterator();it.hasNext();){
+					colName = ((Entry<String,String>)it.next()).getKey();
+					valueMap.put(colName,rs.getString(colName));
+				}
+				valueList.add(valueMap);
+			}
+			//获取pk值
+			for(int i=0;i<valueList.size();i++){
+				pkName = (String) valueList.get(i).get("ETLPKNAME");
+				pkValues.add((String) valueList.get(i).get(pkName));
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally{
-			JdbcUtils.release(null, st, null);
+			throw new RuntimeException("查询数据出错"+e);
+		} finally{
+			JdbcUtils.release(null, st, rs);
 			srcMgr.release(srcConn);
 		}
+		return pkName;
 	}
 	
 	/**
@@ -306,6 +353,7 @@ public class TriggerBaseTask {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally{
+			JdbcUtils.release(null, pst, rs);
 			mgr.release(conn);
 		}
 		tabColMap.put(tableName, result);
@@ -323,7 +371,7 @@ public class TriggerBaseTask {
 	 * @since  2014-10-28
 	 * <p> history 2014-10-28 xiaoh  创建   <p>
 	 */
-	private void enableTrigger(SimpleDSMgr mgr, String tableName, int type, boolean needRelease){
+	private void enableTrigger(SimpleDSMgr mgr, String tableName, int type){
 		
 		if(mgr==null||CommonUtil.isEmpty(tableName)||(type!=0&&type!=1)){
 			throw new RuntimeException("停启用触发器参数出错");
@@ -341,9 +389,8 @@ public class TriggerBaseTask {
 			e.printStackTrace();
 			throw new RuntimeException("停用启用触发器出错"+e);
 		} finally{
-			if(needRelease){
-				mgr.release(conn);
-			}
+			JdbcUtils.release(null, pst, null);
+			mgr.release(conn);
 		}
 	}
 }
