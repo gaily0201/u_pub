52037b17828ddbe7184afab5f4ea5c17123285da
gaily <gaily0201@163.com>
2014-12-03 22:18:07 +0800

    add button test

--- pub/src/test/cn/gaily/pub/test/TestWordDecorator.java
+++ pub/src/test/cn/gaily/pub/test/TestWordDecorator.java
@@ -6,8 +6,13 @@ import java.awt.Toolkit;
 import nc.bs.framework.test.AbstractTestCase;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.ToolBar;
@@ -27,11 +32,39 @@ public class TestWordDecorator extends AbstractTestCase {
 		shell.layout();
 		shell.setSize(d.width, d.height);
 //
-		ToolBar bar = new ToolBar(shell, SWT.NONE);
-		ToolItem item = new ToolItem(bar, SWT.NONE);
-		bar.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false));
-		item.setText("≤‚ ‘π§æﬂ¿∏");
-
+//		ToolBar bar = new ToolBar(shell, SWT.NONE);
+//		ToolItem item = new ToolItem(bar, SWT.NONE);
+//		bar.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false));
+//		item.setText("≤‚ ‘π§æﬂ¿∏");
+//
+//		item.addSelectionListener(new SelectionListener() {
+//			@Override
+//			public void widgetSelected(SelectionEvent arg0) {
+//				System.out.println("abcdefg");
+//			}
+//			
+//			@Override
+//			public void widgetDefaultSelected(SelectionEvent arg0) {
+//				
+//			}
+//		});
+		Button button = new Button(shell,SWT.BORDER);
+		button.setText("≤‚ ‘∞¥≈•");
+		button.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, false, false));
+		button.addMouseListener(new MouseListener() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+			}
+			@Override
+			public void mouseDown(MouseEvent e) {
+				System.out.println("abcdefg");
+			}
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+			}
+		});
+		
+		
 		WordDecorator w = new WordDecorator(shell, SWT.NULL, "c:\\test.doc");
 		w.setLayoutData(new GridData(SWT.FILL,SWT.FILL,true, true));
 		
